74d34c263d232b2dc29ecb3763e63f1fa7fc1142
TonyBimenyi <tonyblaisb@gmail.com>
2025-06-01 22:54:23 +0800

    asdda

---
+++ src/components/footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="footer_container">
+    <h1>This is Footer</h1>
+    <p>© 2023 Your Company Name. All rights reserved.</p>
+    <p>Follow us on:
+      <a href="https://twitter.com/yourcompany" target="_blank">Twitter</a>,
+      <a href="https://facebook.com/yourcompany" target="_blank">Facebook</a>,
+      <a href="https://instagram.com/yourcompany" target="_blank">Instagram</a>
+    </p>
+  </div>
+</template>
---
+++ src/components/header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="header">
+    <div class="logo">
+      <h1>AVe<span class="">NIx</span> </h1>
+    </div>
+    <nav class="nav">
+      <ul>
+        <li>
+            <router-link to="/">Home</router-link>
+        </li>
+        <li>
+            <router-link to="/">About Us </router-link>
+        </li>
+        <li>
+            <router-link to="/">Services</router-link>
+        </li>
+        <li>
+            <router-link to="/">Contact Us</router-link>
+        </li>
+      </ul>
+    </nav>
+    <a href="#" class="donate-btn">Donate Now</a>
+  </header>
+</template>
+<style src="../assets/css/header.css" scoped>
+
+</style>

369933caacf9c488bcdef5f0600ba768050fa4a4
TonyBimenyi <tonyblaisb@gmail.com>
2025-06-01 21:59:16 +0800

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+
+   <nav-bar></nav-bar>
+
   <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import navBar from './components/header.vue';
+export default {
+  name: 'App',
+  components: {
+    navBar
+  },
+  data() {
+    return {
+      // Data properties can be added here if needed
+    };
+  },
+  methods: {
+    // Methods can be added here if needed
   }
 }
+</script>
+
+<style lang="scss">
+
 </style>

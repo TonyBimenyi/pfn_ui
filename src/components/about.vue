855420743eec7c64d923c97972748936fb61aaaf
TonyBimenyi <tonyblaisb@gmail.com>
2025-06-02 19:41:30 +0800

    asdasd

---
+++ src/components/about.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="about_container">
+        <div class="left_part">
+            <div class="img1">
+                <img src="../assets/images/hero-bg.jpg" alt="">
+            </div>
+            <div class="img2">
+                <img src="../assets/images/hero-bg.jpg" alt="">
+            </div>
+        </div>
+        <div class="right_part">
+            <div class="text-icon-container">
+                 <i class="fas fa-arrow-right"></i>
+                <span>ABOUT US</span>        
+            </div>
+            <div class="title_about">
+                Faith, Hope, and Love: <br> <span>A Journey of Compassion </span> 
+            </div>
+
+            <div class="description_about">
+                <p>
+                    We are a vibrant community of believers dedicated to worship, fellowship, and service. Our mission is to share God's love, grow in faith, and make a positive impact in the world through compassionate outreach and meaningful connections.
+                    <br><br>    
+                    Our church is a welcoming place where everyone can find support, inspiration, and a sense of belonging. Together, we strive to live out our faith and make a difference.
+                </p>
+            </div>
+            <div class="icons_about">
+                <div class="left_part_icons">
+                    <div class="text-icon-container">
+                        <i class="fas fa-arrow-right"></i>
+                        <span>ABOUT US</span>        
+                    </div>
+                </div>
+                <div class="right_part_icons">
+                     <div class="text-icon-container">
+                        <i class="fas fa-arrow-right"></i>
+                        <span>ABOUT US</span>        
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</template>
+<style src="../assets/css/about.css" scoped>
+
+</style>
